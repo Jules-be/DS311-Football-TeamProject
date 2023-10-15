3c744cdd9eb6c8e585a0458665582587972da707
Mateo Deroche <mateo.deroche@epitech.eu>
2023-10-15 01:24:23 -0700

    [FIX] clean and simplify code

--- script/data_explorer.py
+++ script/data_explorer.py
@@ -2,44 +2,18 @@ import pandas as pd
 import numpy as np
 import argparse
 
-def find_tournament(file_path):
-    try:
-        df = pd.read_csv(file_path)
-        unique_tournament = df['tournament'].str.strip().unique()
-        return unique_tournament
-    except FileNotFoundError:
-        print("File not found: {file_path}")
-    except Exception as e:
-        print("An error occurred: {str(e)}")
+file = "data/fifa_world_cup.csv"
 
-def find_country(file_path):
-    try:
-        df = pd.read_csv(file_path)
-        unique_country = df['country'].str.strip().unique()
-        return unique_country
-    except FileNotFoundError:
-        print("File not found: {file_path}")
-    except Exception as e:
-        print("An error occurred: {str(e)}")
+def find_tournament():
+    df = pd.read_csv(file)
+    unique_tournament = df['tournament'].str.strip().unique()
+    print(unique_tournament)
 
-def main():
-    parser = argparse.ArgumentParser(description="Extract unique tournament names and countries from a CSV file.")
-    parser.add_argument("csv_file", help="Path to the CSV file")
-    args = parser.parse_args()
-    csv_file_path = args.csv_file
-
-    tournaments = find_tournament(csv_file_path)
-    countries = find_country(csv_file_path)
-
-    if tournaments.size > 0:
-        print("Unique Tournament Names:")
-        for name in tournaments:
-            print(name)
-
-    if countries.size > 0:
-        print("Unique Countries:")
-        for country in countries:
-            print(country)
+def find_country():
+    df = pd.read_csv(file)
+    unique_country = df['country'].str.strip().unique()
+    print(unique_country)
 
 if __name__ == "__main__":
-    main()
+    find_tournament()
+    find_country()
