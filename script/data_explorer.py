02fb11484129fb69429092d541d3a0d413e47bc4
Mateo Deroche <mateo.deroche@epitech.eu>
2023-10-15 01:17:22 -0700

    [ADD] add proper library

--- script/data_explorer.py
+++ script/data_explorer.py
@@ -1,65 +1,45 @@
-import csv
+import pandas as pd
+import numpy as np
 import argparse
 
 def find_tournament(file_path):
-    tournament_names = set()
-
     try:
-        with open(file_path, newline='') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                tournament_name = row['tournament'].strip()
-                tournament_names.add(tournament_name)
-
+        df = pd.read_csv(file_path)
+        unique_tournament = df['tournament'].str.strip().unique()
+        return unique_tournament
     except FileNotFoundError:
         print("File not found: {file_path}")
     except Exception as e:
         print("An error occurred: {str(e)}")
-
-    return list(tournament_names)
 
 def find_country(file_path):
-    countries = set()
-
     try:
-        with open(file_path, newline='') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                country = row['country'].strip()
-                countries.add(country)
-
+        df = pd.read_csv(file_path)
+        unique_country = df['country'].str.strip().unique()
+        return unique_country
     except FileNotFoundError:
         print("File not found: {file_path}")
     except Exception as e:
         print("An error occurred: {str(e)}")
 
-    return list(countries)
-
 def main():
-    parser = argparse.ArgumentParser(description="Extract unique tournament names from a CSV file.")
+    parser = argparse.ArgumentParser(description="Extract unique tournament names and countries from a CSV file.")
     parser.add_argument("csv_file", help="Path to the CSV file")
-
     args = parser.parse_args()
     csv_file_path = args.csv_file
 
-    tournament_names = find_tournament(csv_file_path)
+    tournaments = find_tournament(csv_file_path)
     countries = find_country(csv_file_path)
 
-    if tournament_names:
-        print("Tournament in the list:")
-        for name in tournament_names:
+    if tournaments.size > 0:
+        print("Unique Tournament Names:")
+        for name in tournaments:
             print(name)
-    else:
-        print("No tournament found in the CSV file.")
 
-    if countries:
-        print("\n\nCountry in the list:")
+    if countries.size > 0:
+        print("Unique Countries:")
         for country in countries:
             print(country)
-    else:
-        print("No country found in the CSV file.")
-
-        
 
 if __name__ == "__main__":
     main()
